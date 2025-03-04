fb2bc0ceb72f5d21124accd991d376d73b469e2f
Chris <rewindyourtape@gmail.com>
2025-03-04 14:03:07 -0700

    adding animejs types and disabling routing for now

--- cwatland-lt.github.io/src/main.tsx
+++ cwatland-lt.github.io/src/main.tsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Provider} from '@/components/ui/provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <Provider>
       <BrowserRouter>
         <Routes>
-            <Route path="./" element={<App />} />
+            <Route path="/" element={<App />} />
         </Routes>
       </BrowserRouter>
-    <App />
+      </Provider>
   </StrictMode>,
 )
